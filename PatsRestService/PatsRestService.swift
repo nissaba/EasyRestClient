df4ec63671f0304ae6ea382d6fc04d11a0dead1b
Pascale Beaulac <pascale.beaulac@argon18.com>
2025-04-28 17:07:22 -0400

    Initial Commit

---
+++ PatsRestService/PatsRestService.swift
@@ -0,0 +1,9 @@
+//
+//  PatsRestService.swift
+//  PatsRestService
+//
+//  Created by Pascale on 2025-04-28.
+//
+
+import Foundation
+
---
+++ PatsRestServiceTests/PatsRestServiceTests.swift
@@ -0,0 +1,17 @@
+//
+//  PatsRestServiceTests.swift
+//  PatsRestServiceTests
+//
+//  Created by Pascale on 2025-04-28.
+//
+
+import Testing
+@testable import PatsRestService
+
+struct PatsRestServiceTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
