7d8f264900601c1a588dda2c646415920683fb5c
Pascale Beaulac <pascale.beaulac@argon18.com>
2025-04-28 20:51:35 -0400

    Add ExampleApp

---
+++ Examples/ExampleApp/ExampleApp/ExampleAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExampleAppApp.swift
+//  ExampleApp
+//
+//  Created by Pascale on 2025-04-28.
+//
+
+import SwiftUI
+
+@main
+struct ExampleAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Examples/ExampleApp/ExampleAppTests/ExampleAppTests.swift
@@ -0,0 +1,16 @@
+//
+//  ExampleAppTests.swift
+//  ExampleAppTests
+//
+//  Created by Pascale on 2025-04-28.
+//
+
+import Testing
+
+struct ExampleAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
